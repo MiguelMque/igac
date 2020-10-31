12c1d204e168d201622beabf9a86000dd3fd2fef
esgomezco-ds4 <esgomezco@hotmail.com>
2020-10-31 14:58:19 -0500

    cambios en main, y el patch de make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,9 +6,9 @@ def leer_csv_fr(data_path):
 
 
 def leer_csv_datosigac(data_path):
-    df=pd.read_csv(data_path+'AvaluoCatastral_JULIO312020.csv',sep=";")
-    df2=pd.read_csv(data_path+'TOTAL_NACIONAL_REG1.txt',sep="\t",encoding='latin-1')
-    df3=pd.read_csv(data_path+"TOTAL_NACIONAL_REG2.txt",sep="\t",encoding="latin-1")
-    df4=pd.read_csv(data_path+"destino_economico.csv",sep=";")
+    df=pd.read_csv(data_path+'/AvaluoCatastral_JULIO312020.csv',sep=";")
+    df2=pd.read_csv(data_path+'/TOTAL_NACIONAL_REG1.txt',sep="\t",encoding='latin-1')
+    df3=pd.read_csv(data_path+"/TOTAL_NACIONAL_REG2.txt",sep="\t",encoding="latin-1")
+    df4=pd.read_csv(data_path+"/destino_economico.csv",sep=";")
     return df,df2,df3,df4
 
